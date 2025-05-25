e04c22d77ee22761e3d9cdc0b42b0f781c642d4c
br0sinski <bastian.rosinski@hotmail.com>
2025-05-25 13:02:58 +0200

    add: own logger file

--- MonaaLisa/src/SemanticPaper/logger/logger.py
+++ MonaaLisa/src/SemanticPaper/logger/logger.py
@@ -1,2 +1,22 @@
 import logging
 
+def setup_logger(logfile='latest_semanticpaper.log'):
+    """
+    Sets up logging to both file (overwrite on each run) and console.
+    Returns a logger instance.
+    """
+    logger = logging.getLogger("SemanticPaper")
+    logger.setLevel(logging.INFO)
+
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    file_handler = logging.FileHandler(logfile, mode='w', encoding='utf-8')
+    file_formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(file_formatter)
+    logger.addHandler(console_handler)
+
+    return logger
