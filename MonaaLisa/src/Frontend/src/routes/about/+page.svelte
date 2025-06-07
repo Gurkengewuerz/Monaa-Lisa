9c7c5c2e2f881c688a576ea91e8987cbacf8b1b5
br0sinski <bastian.rosinski@hotmail.com>
2025-06-07 10:50:54 +0200

    add: cimple component examples

---
+++ MonaaLisa/src/Frontend/src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import SampleComponent from "./SampleComponent.svelte";
+
+    let current = 0;
+
+    function addCounter() {
+        current++;
+    }
+</script>
+<!-- Bind the component here - 7th June 2025 - Basti -->
+<SampleComponent/>
+<h1>About Monaa Lisa</h1>
+<p>Current Count: {current}</p>
+<button on:click={addCounter}>Click me!</button>
