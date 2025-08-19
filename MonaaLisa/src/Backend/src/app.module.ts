d752ca7fbb9f79136a4f9c4027cb39784d29ce2e
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2025-08-19 15:47:12 +0200

    prisma service created

--- MonaaLisa/src/Backend/src/app.module.ts
+++ MonaaLisa/src/Backend/src/app.module.ts
@@ -4,12 +4,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 // Das ist unser Modul. Module enthalten Struktur aber keinen Code selbst.
 // Der typische ablauf ist Client->Controller->Service->(DB)->Service->Controller->Client
 @Module({
   imports: [], // andere @Module die intern benötigt werdfen
   controllers: [AppController], // Alle Controller die dem Modul zugeordnet sind. Nimmt Requests an
-  providers: [AppService], // Logik. Verwendet Datenbanken, Validierung usw. Wird vom Controller aufgerufen
+  providers: [AppService, PrismaService], // Logik. Verwendet Datenbanken, Validierung usw. Wird vom Controller aufgerufen
 })
 export class AppModule {}
